8cdea758954e4f96ae917fd96caa8692654d8f91
Your Name <you@example.com>
2023-11-28 09:12:41 -0600

    fixed the links for test cases

--- app/tests.py
+++ app/tests.py
@@ -10,15 +10,15 @@ from django.test import SimpleTestCase
 class testNearestHund(SimpleTestCase):
     def test_near_hund_89(self):
         response = self.client.get("/warmup-1/near-hundred/89/")
-        self.assertContains(response, False)
+        self.assertContains(response, "False")
 
     def test_near_hund_90(self):
         response = self.client.get("/warmup-1/near-hundred/90/")
-        self.assertContains(response, True)
+        self.assertContains(response, "True")
 
     def test_near_hund_93(self):
         response = self.client.get("/warmup-1/near-hundred/93/")
-        self.assertContains(response, True)
+        self.assertContains(response, "True")
 
 
 class testStringSplosion(SimpleTestCase):
@@ -37,27 +37,27 @@ class testStringSplosion(SimpleTestCase):
 
 class testString2(SimpleTestCase):
     def test_cat(self):
-        response = self.client.get("/string-2/catdog/")
+        response = self.client.get("/string-2/cat-dog/catdog/")
         self.assertContains(response, True)
 
     def test_catCat(self):
-        response = self.client.get("/string-2/catcat/")
+        response = self.client.get("/string-2/cat-dog/catcat/")
         self.assertContains(response, False)
 
     def test_catyaddayadda(self):
-        response = self.client.get("/string-2/1cat1cadodog/")
+        response = self.client.get("/string-2/cat-dog/1cat1cadodog/")
         self.assertContains(response, True)
 
 
 class testLonesomenumber(SimpleTestCase):
     def test1_2_3(self):
-        response = self.client.get("/Logic-2/1/2/3/")
+        response = self.client.get("/Logic-2/lone-sum/1/2/3/")
         self.assertContains(response, 6)
 
     def test3_2_3(self):
-        response = self.client.get("/Logic-2/3/2/3/")
+        response = self.client.get("/Logic-2/lone-sum/3/2/3/")
         self.assertContains(response, 2)
 
     def test3_3_3(self):
-        response = self.client.get("/Logic-2/3/3/3/")
+        response = self.client.get("/Logic-2/lone-sum/3/3/3/")
         self.assertContains(response, 0)
